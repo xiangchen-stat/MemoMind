c52b9bde156009c9f57aa0d76bdd33ef4e54fcfc
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2024-02-20 20:08:25 -0800

    fixed the routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
 import LoginSignup from './Components/LoginSignup/LoginSignup';
-import NotesApp from './NotesApp'; // Adjust the path as necessary
+import NotesApp from './Components/NotesApp/NotesApp';
+import NoteManager from './Components/NoteManager/NoteManager';
 import './App.css';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <Routes> {/* Use Routes here */}
         <Route path="/" element={<LoginSignup />} exact />
-        <Route path="/notes" element={<NotesApp />} />
+        <Route path="/Notes" element={<NotesApp />} />
         <Route path="/manage-notes" element={<NoteManager />} />
       </Routes>
     </Router>
