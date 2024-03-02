9fb231532e87e46c4c9c040dfa803f1fb6b0ef7a
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2024-03-01 21:02:53 -0800

    removes userData when logging out, that way you cant go back to the other pages

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,14 +1,25 @@
 // Layout.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import './Layout.css';
+import { useDispatch } from 'react-redux';
+import { logoutSuccess } from '../../LoginStore/authSlice';
 
 const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('userData');
+    dispatch(logoutSuccess());
+    navigate('/');
+  };
+
   return (
     <div className="app-container">
       <div className="sidebar">
-        <Link to="/">Logout</Link>
+        <button onClick={handleLogout} className="logout-button">Logout</button>
         <NavLink to="/Notes" className="nav-link" activeClassName="active">Notes</NavLink>
         <NavLink to="/Calendar" className="nav-link" activeClassName="active">Calendar</NavLink>
       </div>
