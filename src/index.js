2965e13e92527aeff376dd66fe97513050e06bae
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2024-03-01 21:12:16 -0800

    checks if user data stored in local storage, dispatch login action with parsed user data to update the redux store. Basically maintains user session

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './LoginStore/store';
+import { loginSuccess } from './LoginStore/authSlice';
+
+const userData = localStorage.getItem('userData');
+if (userData) {
+  store.dispatch(loginSuccess(JSON.parse(userData)));
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
