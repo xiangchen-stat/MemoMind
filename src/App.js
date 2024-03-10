f194cd4fac2f962ba7463810b18be71a1bb14b05
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2024-03-09 22:30:26 -0800

    added path to privacy manager, friend maanger, and friends notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import NotesApp from './Components/NotesApp/NotesApp';
-import NoteManager from './Components/NoteManager/NoteManager';
+import PrivacyManager from './Components/PrivacyManager/PrivacyManager';
 import Calendar from './Components/Calendar/Calendar';
 import CustomEditor from './Components/NotesApp/CustomEditor';
-import './App.css';
 import Layout from './Components/Layout/Layout';
 import ProtectedRoute from './LoginStore/ProtectedRoute.js'; 
+import FriendManager from './Components/FriendManager/FriendManager'
+import FriendNotes from './Components/FriendManager/FriendNotes';
+import './App.css';
+
 
 function App() {
   return (
@@ -18,18 +22,27 @@ function App() {
           <ProtectedRoute>
             <Layout><NotesApp /></Layout>
           </ProtectedRoute>} />
-        <Route path="/manage-notes" element={
-          <ProtectedRoute>
-            <Layout><NoteManager /></Layout>
-          </ProtectedRoute>} />
         <Route path="/calendar" element={
           <ProtectedRoute>
             <Layout><Calendar /></Layout>
           </ProtectedRoute>} />
-          <Route path="/Editor" element={
+        <Route path="/Editor" element={
           <ProtectedRoute>
             <Layout><CustomEditor /></Layout>
           </ProtectedRoute>} />
+        <Route path="/PrivacyManager" element={
+          <ProtectedRoute>
+            <Layout><PrivacyManager /></Layout>
+          </ProtectedRoute>} />
+        <Route path="/FriendManager" element={
+          <ProtectedRoute>
+            <Layout><FriendManager /></Layout>
+          </ProtectedRoute>} />
+        <Route path="/FriendsNotes/:friendEmail" element={
+          <ProtectedRoute>
+            <Layout><FriendNotes /></Layout>
+          </ProtectedRoute>
+        } />
       </Routes>
     </Router>
   );
