48ec75a7ccc267f82b942daa19125582f3f392c2
sharonc05 <152813226+sharonc05@users.noreply.github.com>
2024-03-10 23:40:55 -0700

    Create setup.sh
    Bash script to run project after cloning repository and cd MemoMind

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# first clone the repository and cd MemoMind
+
+# Install npm dependencies
+npm install
+
+# Run the development server
+npm run dev
+
+# make sure to make script executable by doing chmod +x setup.sh in terminal before running with ./setup.sh
